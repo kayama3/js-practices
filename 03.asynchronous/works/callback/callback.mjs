c8cd37056bdae4571871a3d9b1520b83bd4a051f
kayama <shimonk333@gmail.com>
2023-09-08 16:50:23 +0900

    INSERTと同時にidを取得するように変更

--- 03.asynchronous/works/callback/callback.mjs
+++ 03.asynchronous/works/callback/callback.mjs
@@ -7,33 +7,23 @@ db.run(
   () => {
     db.run(
       "INSERT INTO books (title) VALUES('オブジェクト指向設計実践ガイド')",
-      () => {
-        db.get(
-          "SELECT * FROM books WHERE title = 'オブジェクト指向設計実践ガイド'",
-          (err, row) => {
-            console.log(row.id);
+      function () {
+        console.log(this.lastID);
 
-            db.run(
-              "INSERT INTO books (title) VALUES('Webを支える技術')",
-              () => {
-                db.get(
-                  "SELECT * FROM books WHERE title = 'Webを支える技術'",
-                  (err, row) => {
-                    console.log(row.id);
+        db.run(
+          "INSERT INTO books (title) VALUES('Webを支える技術')",
+          function () {
+            console.log(this.lastID);
 
-                    db.all("SELECT * FROM books", (err, rows) => {
-                      rows.forEach((row) => {
-                        console.log(row.id, row.title);
-                      });
+            db.all("SELECT * FROM books", (err, rows) => {
+              rows.forEach((row) => {
+                console.log(row.id, row.title);
+              });
 
-                      db.run("DROP TABLE books", () => {
-                        db.close();
-                      });
-                    });
-                  }
-                );
-              }
-            );
+              db.run("DROP TABLE books", () => {
+                db.close();
+              });
+            });
           }
         );
       }
