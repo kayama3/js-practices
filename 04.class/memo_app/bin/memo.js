77b1858ec270a695833948f6c9c8feaa12fcecfe
kayama <shimonk333@gmail.com>
2023-10-18 16:48:28 +0900

    memo.jsの指摘箇所修正

--- 04.class/memo_app/bin/memo.js
+++ 04.class/memo_app/bin/memo.js
@@ -1,9 +1,8 @@
-import { Command } from "../lib/command.js";
 import minimist from "minimist";
 import sqlite3 from "sqlite3";
-const argv = minimist(process.argv.slice(2));
-const dataBase = new sqlite3.Database("|memory|");
+import { Command } from "../lib/command.js";
 
+const argv = minimist(process.argv.slice(2));
+const dataBase = new sqlite3.Database('memo.db');
 const command = new Command(argv, dataBase);
 command.exec();
-
